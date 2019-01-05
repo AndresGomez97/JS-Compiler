5f841de01a58a4e96bcfc105aeda994cdc1611cb
AndrewGomek <gomekito@gmail.com>
2019-01-05 21:41:30 +0100

    Expresiones NOT,MENORQUE y SUMA

--- parse.py
+++ parse.py
@@ -3,20 +3,35 @@ import ply.yacc as yacc
  # Get the token map from the lexer.  This is required.
 from AL import tokens
 
+def p_not_erre(p):
+    'eee : NEG erre'
+    p[0] = not p[2]
+
+def p_eee_erre(p):
+    'eee : erre'
+    p[0] = p[1]
+
+def p_erre_expression_minusthan(p):
+    'erre : erre MENORQUE expression'
+    p[0] = p[1] < p[3]
+    
+        
+def p_erre_expression(p):
+    'erre : expression'
+    p[0] = p[1]
+
 def p_expression_plus(p):
-    'expression : expression SUM term'
+    'expression : term SUM expression'
     p[0] = p[1] + p[3]
 
 def p_expression_term(p):
     'expression : term'
     p[0] = p[1]
-     
+    
 def p_term_number(p):
     'term : ENTERO'
     p[0] = p[1]
 
-
-    
 def p_error(p):
      print("Syntax error in input!")
 
